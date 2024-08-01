a0236dc0aaa1342b964527a7aa068b9a737c81fb
Robert <rspatz@live.com>
2024-07-31 20:45:03 -0500

    Begin building mobile navigation

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Mobile Navigation
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visibility = mainNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    mainNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    mainNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+});
